3e74947c68bd22a33fd7ed556fb863ffb65423a2
Aegen <kyuzo101@gmail.com>
2013-04-12 00:16:59 -0500

    Update main.cpp
    Simple style changes

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ vector< vector<int> > board(8);
 int bannedX[8] = {9,9,9,9,9,9,9,9};
 int bannedY[8] = {9,9,9,9,9,9,9,9};
 
-inline bool isBanned(int _x, int _y){
+inline bool isBanned(int _x, int _y)
+{
     for(int a = 0; a < 8; a++){
         if( _x == bannedX[a] || _y == bannedY[a])
             return true;
@@ -21,18 +22,22 @@ inline bool isBanned(int _x, int _y){
     return false;
 }
 
-inline bool isCorrect(){
+inline bool isCorrect()
+{
     bool correct = true;
 
     int x;
     int y;
 
-    for(int a = 0; a < 7; a++){
-        for(int b = a + 1; b < 8; b++){
+    for(int a = 0; a < 7; a++)
+    {
+        for(int b = a + 1; b < 8; b++)
+        {
             x = abs(bannedX[a] - bannedX[b]);
             y = abs(bannedY[a] - bannedY[b]);
 
-            if(x == y){
+            if(x == y)
+            {
                 correct = false;
             }
 
@@ -42,15 +47,19 @@ inline bool isCorrect(){
     return correct;
 }
 
- inline void resetBoard(){
+ inline void resetBoard()
+ {
     for (int a = 0; a < 8; a++)
         for(int b = 0; b < 8; b++)
             board.at(a).at(b) = 0;
 }
 
-inline void addToBanned(int _x, int _y){
-    for(int a =0; a < 8; a++){
-        if(bannedX[a] == 9){
+inline void addToBanned(int _x, int _y)
+{
+    for(int a =0; a < 8; a++)
+    {
+        if(bannedX[a] == 9)
+        {
             bannedX[a] = _x;
             bannedY[a] = _y;
             a = 8;
@@ -58,23 +67,30 @@ inline void addToBanned(int _x, int _y){
     }
 }
 
-inline void resetBanned(){
-    for(int a = 0; a < 8; a++){
+inline void resetBanned()
+{
+    for(int a = 0; a < 8; a++)
+    {
         bannedX[a] = 9;
         bannedY[a] = 9;
     }
 }
 
-inline void initializeBoard(){
+inline void initializeBoard()
+{
     for(int a = 0; a < 8; a++)
         board.at(a).resize(8);
+        
     resetBoard();
 }
 
-inline void printBoard(){
-    for(int a = 0; a < 8; a++){
+inline void printBoard()
+{
+    for(int a = 0; a < 8; a++)
+    {
         for(int b = 0; b < 8; b++)
             cout << board.at(a).at(b) << " ";
+            
         cout << endl;
     }
 }
@@ -92,22 +108,22 @@ int main()
 
     bool done = false;
 
-    while(done == false){
-
+    while(done == false)
+    {
         resetBoard();
         resetBanned();
 
-        for(int d = 0; d < 8;){
+        for(int d = 0; d < 8;)
+        {
 
             x = rand() % 8;
             y = rand() % 8;
 
-            if(!isBanned(x,y)){
-
+            if(!isBanned(x,y))
+            {
                 board[x][y] = 1;
                 addToBanned(x,y);
                 d++;
-
             }
         }
 
